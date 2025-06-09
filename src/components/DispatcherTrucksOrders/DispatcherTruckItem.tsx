73de58c501d978249c906914f0268fbd353acd85
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-06-09 14:25:20 -0600

    trucks with expandable orders done

---
+++ src/components/DispatcherTrucksOrders/DispatcherTruckItem.tsx
@@ -0,0 +1,40 @@
+import type { Truck } from "../../interfaces/TruckInterface";
+import styles from './dispatcherTruckItem.module.css'
+import { useState } from "react";
+import type { Order } from "../../interfaces/OrderInterface";
+
+
+interface TruckItemProps {
+    truckItem: Truck;
+    truckOrders: Order[];
+    setUpdateTrucksOrders: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+
+
+export default function DispatcherTruckItem({ truckItem, truckOrders, setUpdateTrucksOrders }: TruckItemProps) {
+    const [expand, setExpand] = useState(false);
+    //console.log(truckOrders);
+    
+    function displayTruckOrders() {
+        return truckOrders.map(it => <div className={styles.divOrder}>
+            Dest: {it.destination} &nbsp;
+            W: {it.weight}
+        </div>)
+    }
+
+    const currentCapacity = truckItem.capacity - truckOrders.reduce((acc, it) => acc+ it.weight,0);
+
+    return (
+        <>
+            <div className={currentCapacity<0?styles.divOverload:null}>
+                <button onClick={() => { setExpand(s => !s) }}>{!expand ? '⇓' : '⇑'}</button>
+                {truckItem.licensePlate} &nbsp;
+                TC: {truckItem.capacity} &nbsp;
+                CC: {currentCapacity}
+                {expand && displayTruckOrders()}
+            </div>
+
+        </>
+    )
+}
--- src/pages/dashboards/DispatcherDashboard.tsx
+++ src/pages/dashboards/DispatcherDashboard.tsx
@@ -1,14 +1,62 @@
+import { useEffect, useState } from "react";
 import DispatcherNav from "../../components/Nav/DispatcherNav";
 import styles from './dashboard.module.css'
+import type { Truck } from "../../interfaces/TruckInterface";
+import type { Order } from "../../interfaces/OrderInterface";
+import { useAuth } from "../../context/authContext/authContext";
+import api from "../../api";
+import DispatcherTruckItem from "../../components/DispatcherTrucksOrders/DispatcherTruckItem";
 
 export default function DispatcherDashBoard() {
+
+    const { cookies } = useAuth();
+    const [trucks, setTrucks] = useState<Truck[]>([]);
+    const [orders, setOrders] = useState<Order[]>([]);
+    const [updateTrucksOrders, setUpdateTrucksOrders] = useState(false)
+
+    useEffect(() => {
+        async function getTrucksOrders() {
+            try {
+                const [resTrucks, resOrders] = await Promise.all([
+                    api.get(`/trucks/available`, { headers: { token: cookies.token } }), //getting only available trucks
+                    api.get(`/orders/pendingassignedorders`, { headers: { token: cookies.token } }) //getting all pending and assigned orders
+                ]);
+                setOrders(resOrders.data);
+                setTrucks(resTrucks.data);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        getTrucksOrders();
+    }, [updateTrucksOrders])
+
+    function displayTrucks() {
+        return trucks?.map(truck => {
+
+            return <DispatcherTruckItem
+                truckItem={truck}
+                truckOrders={orders?.filter(order => order.truck?._id == truck._id)}
+                setUpdateTrucksOrders={setUpdateTrucksOrders}
+                key={truck._id}
+            />
+        })
+    }
+
     return (
         <>
             <DispatcherNav />
-             <div className={styles.divMain}>
-                <div className={`${styles.divTrucks} ${styles.resizable}`}>Trucks</div>
-                <div className={`${styles.divTrucks} ${styles.resizable}`}>Orders</div>
-                <div className={`${styles.divTrucks} ${styles.resizable}`}>Map</div>     
+            <div className={styles.divMain}>
+                <div className={`${styles.divTrucks} ${styles.resizable}`}>
+                    Trucks
+                    {!trucks?.length ? <h2>Loading...</h2> : displayTrucks()}
+
+                </div>
+                <div className={`${styles.divTrucks} ${styles.resizable}`}>
+                    Orders
+                </div>
+                <div className={`${styles.divTrucks} ${styles.resizable}`}>
+                    Map
+                </div>
 
             </div>
         </>
