eeba5f53f596fbfd1e25c72258631f094ba10d24
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-06-10 14:36:38 -0600

    colors array added

---
+++ src/components/Map/colors.ts
@@ -0,0 +1,53 @@
+const routeColors = [
+  '#FF0000',    // Bright Red
+  '#00FF00',    // Lime Green
+  '#0000FF',    // Bright Blue
+  '#FFFF00',    // Yellow
+  '#FF00FF',    // Magenta
+  '#00FFFF',    // Cyan
+  '#800000',    // Dark Red
+  '#008000',    // Dark Green
+  '#000080',    // Navy Blue
+  '#808000',    // Olive
+  '#800080',    // Purple
+  '#008080',    // Teal
+  '#C0C0C0',    // Silver
+  '#FFA500',    // Orange
+  '#A52A2A',    // Brown
+  '#8A2BE2',    // Blue Violet
+  '#5F9EA0',    // Cadet Blue
+  '#7FFF00',    // Chartreuse
+  '#D2691E',    // Chocolate
+  '#FF7F50',    // Coral
+  '#6495ED',    // Cornflower Blue
+  '#DC143C',    // Crimson
+  '#00CED1',    // Dark Turquoise
+  '#9400D3',    // Dark Violet
+  '#FF1493',    // Deep Pink
+  '#00BFFF',    // Deep Sky Blue
+  '#696969',    // Dim Gray
+  '#1E90FF',    // Dodger Blue
+  '#B22222',    // Firebrick
+  '#228B22',    // Forest Green
+  '#DAA520',    // Goldenrod
+  '#ADFF2F',    // Green Yellow
+  '#FF69B4',    // Hot Pink
+  '#CD5C5C',    // Indian Red
+  '#4B0082',    // Indigo
+  '#F08080',    // Light Coral
+  '#20B2AA',    // Light Sea Green
+  '#90EE90',    // Light Green
+  '#ADD8E6',    // Light Blue
+  '#778899',    // Light Slate Gray
+  '#B0C4DE',    // Light Steel Blue
+  '#00FA9A',    // Medium Spring Green
+  '#9370DB',    // Medium Purple
+  '#3CB371',    // Medium Sea Green
+  '#7B68EE',    // Medium Slate Blue
+  '#00FF7F',    // Spring Green
+  '#4682B4',    // Steel Blue
+  '#D2B48C',    // Tan
+  '#EE82EE',    // Violet
+  '#9ACD32'     // Yellow Green
+];
+export default routeColors;
